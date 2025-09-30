f5911da124087d12a15abfdba82e6e595acc998c
YusufTrgt <yusuf_turgut_02@hotmail.de>
2025-10-01 00:51:27 +0200

    Muenzen Skript edited

---
+++ Scripts/MuenzeScripts/Muenze.cs
@@ -0,0 +1,67 @@
+using Godot;
+using System;
+
+public partial class Muenze : Area2D
+{
+	[Export] public int CoinValue = 1; // Wie viel die Münze wert ist
+	
+	// Optional: Sound effect beim Aufsammeln
+	// [Export] public AudioStream collectSound;
+	// private AudioStreamPlayer2D audioPlayer;
+
+	public override void _Ready()
+	{
+		// Verbinde das Signal für Kollision
+		BodyEntered += OnBodyEntered;
+		
+		// Optional: Audio Player setup
+		// if (collectSound != null)
+		// {
+		//     audioPlayer = new AudioStreamPlayer2D();
+		//     audioPlayer.Stream = collectSound;
+		//     AddChild(audioPlayer);
+		// }
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		// Prüfe ob es der Player ist
+		if (body is Player player)
+		{
+			// Sammle die Münze ein
+			CollectCoin(player);
+		}
+	}
+
+	private void CollectCoin(Player player)
+	{
+		GD.Print($"+{CoinValue} coin(s)");
+		
+		// TODO: Hier Score zum Player/ScoreManager hinzufügen
+		// Beispiel wenn ScoreManager als Singleton existiert:
+		// ScoreManager.Instance.AddPoints(CoinValue);
+		
+		// Optional: Sound abspielen
+		// if (audioPlayer != null)
+		// {
+		//     audioPlayer.Play();
+		//     // Warte bis Sound fertig ist, dann lösche
+		//     await ToSignal(audioPlayer, AudioStreamPlayer2D.SignalName.Finished);
+		// }
+		
+		// Optional: Particle Effect oder Animation vor dem Löschen
+		// PlayCollectAnimation();
+		
+		// Lösche die Münze
+		QueueFree();
+	}
+	
+	// Optional: Collect Animation
+	// private async void PlayCollectAnimation()
+	// {
+	//     // Fade out oder Scale animation
+	//     Tween tween = CreateTween();
+	//     tween.TweenProperty(this, "scale", Vector2.Zero, 0.3f);
+	//     await ToSignal(tween, Tween.SignalName.Finished);
+	// }
+}
