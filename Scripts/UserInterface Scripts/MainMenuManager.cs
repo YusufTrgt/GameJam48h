ceb4607113e4acedaed52ffa57dc80aed6e74c46
sozialday <eyo1232004@gmail.com>
2025-09-30 16:12:51 +0300

    Das ist alles zum Hauptmenu fuer weiteres einfach melden

---
+++ Scripts/UserInterface Scripts/MainMenuManager.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+// folgende klasse ist zustaendig fuer die interaktion mit den 
+// Buttons im Hauptmenu : einfachhaltshalber wird direkt hier auch die 
+// Logik implementiert
+
+public partial class MainMenuManager : Node
+{
+	[Export] public Button quitButton;
+	[Export] public Button startButton;
+	
+	 public override void _Ready()
+	{
+		startButton.Pressed += OnStartButtonPressed;
+		quitButton.Pressed += OnQuitButtonPressed;
+	}
+
+	// schliesst die applikation
+	private void OnQuitButtonPressed()
+	{
+	   GetTree().Quit();
+	}
+
+	// oeffnet das erste level
+	private void OnStartButtonPressed()
+	{
+		GD.Print("spiel gestartet !!!");
+	}
+}
