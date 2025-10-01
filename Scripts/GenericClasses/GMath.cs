2aa2fba3055eb04f6deebbccd80a3ddbfb25c317
sozialday <eyo1232004@gmail.com>
2025-10-01 13:08:13 +0300

    Generische Klasse welche ein paar 1 statische methoden haelt

---
+++ Scripts/GenericClasses/GMath.cs
@@ -0,0 +1,31 @@
+using System;
+using Godot;
+
+public static class GMath
+{
+	// Interpolations Methode [sauber bewegung zwischen 2 punkten]
+	public static float FInterpTo(float A, float B, float DeltaSeconds, 
+	float InterpolationSpeed = 6)
+	{
+		if (InterpolationSpeed <= 0f || DeltaSeconds <= 0f)
+			return B;
+
+		float diff = B - A;
+		if (Math.Abs(diff) < 1e-6f)
+			return B;
+
+		float delta = DeltaSeconds * InterpolationSpeed;
+		
+		if (delta > 1f) delta = 1f;
+			return A + diff * delta;
+	}
+	
+	public static Vector2 VInterpTo(Vector2 A, Vector2 B, float DeltaSeconds,
+	float InterpolationSpeed = 6)
+	{
+		 return new Vector2(
+			FInterpTo(A.X, B.X, DeltaSeconds, InterpolationSpeed),
+			FInterpTo(A.Y, B.Y, DeltaSeconds, InterpolationSpeed)
+		);
+	}
+}
