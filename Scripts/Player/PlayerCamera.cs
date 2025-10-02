124e09bad7c99610992d6953fce3fb163ea6568f
Nawied Khaleqi <74689002+nawiedk@users.noreply.github.com>
2025-10-02 09:00:23 +0200

    Limited Camera Movement Downwards

--- Scripts/Player/PlayerCamera.cs
+++ Scripts/Player/PlayerCamera.cs
@@ -5,18 +5,31 @@ public partial class PlayerCamera : Camera2D
 	[Export] private CharacterBody2D target;
 	[Export] private Vector2 offset = Vector2.Zero;
 
+	private float startY;
+	private float maxFallDistance = 10f;
+
 	public override void _Ready()
 	{
 		if (target != null)
 		{
 			GlobalPosition = target.GlobalPosition + offset;
 		}
+		startY = GlobalPosition.Y;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		if (target == null) return;
-		GlobalPosition = target.GlobalPosition + offset;
+
+		Vector2 newPosition = target.GlobalPosition + offset;
+
+		// Limtiert die Kamera in der Y Achse
+		if (newPosition.Y > startY + maxFallDistance)
+		{
+			newPosition.Y = startY + maxFallDistance;
+		}
+
+		GlobalPosition = newPosition;
 	}
 
 	public void SetTarget(CharacterBody2D newTarget)
